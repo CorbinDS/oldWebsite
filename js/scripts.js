3a7c7e14249eed21f2f8fe613ce1b850ad1f2591
CorbinDS <cds244@cornell.edu>
2024-03-31 16:56:36 -0400

    work

---
+++ js/scripts.js
@@ -0,0 +1,67 @@
+window.onload = function () {
+  const defaultTypeSpeed = 50;
+  const defaultFadeSteps = 30;
+  const defaultFadeDelay = 10;
+
+  function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+  const introAnimation = async function () {
+    let iText = document.getElementById("introText");
+    let subText = document.getElementById("hideUntilDone");
+    let centeredDiv = document.getElementsByClassName("centered");
+    let everythingElse = document.getElementsByClassName("everything");
+
+    await delay(1500);
+    await deleteCurrentText(iText);
+    await delay(100);
+    let msg = "My name is Corbin Stickley.";
+    await typeInText(iText, msg);
+    await delay(500);
+    await deleteCurrentText(iText);
+    await delay(100);
+    msg = "Welcome to my portfolio!"
+    await typeInText(iText, msg);
+    await delay(200);
+
+    // centeredDiv[0].style.display = "none";
+    // everythingElse[0].style.display = "flex";
+
+    // iText.style.visibility = "hidden";
+    // iText.style.display = "none";
+    // subText.style.opacity = 1.0;
+
+    subText.style.opacity = 0.0;
+    await fadeIn(subText);
+
+
+  }
+  introAnimation();
+
+
+  async function deleteCurrentText(textContainer, letterDelay = defaultTypeSpeed) {
+    for (let i = textContainer.textContent.length; i > 0; i--) {
+      textContainer.textContent = textContainer.textContent.slice(0, i);
+      await delay(letterDelay);
+    }
+  }
+
+  async function typeInText(textContainer, newText, letterDelay = defaultTypeSpeed) {
+    for (let i = 0; i < newText.length; i++) {
+      textContainer.textContent = newText.slice(0, i + 1);
+      await delay(letterDelay);
+    }
+  }
+
+  async function fadeIn(htmlItem, fadeSteps = defaultFadeSteps, stepDelay = defaultFadeDelay) {
+    let stepAmount = 1 / fadeSteps;
+    let currentOpacity = Number(htmlItem.style.opacity);
+    for (let i = 0; i < fadeSteps; i++) {
+      currentOpacity = currentOpacity + stepAmount;
+      htmlItem.style.opacity = currentOpacity;
+      await delay(stepDelay)
+    }
+  }
+
+}
